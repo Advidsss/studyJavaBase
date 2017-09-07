8cca1570712949b2cebe20eb594e76d745af27fc
YOUR NAME <YOU@EXAMPLE.COM>
2017-09-07 23:56:59 +0800

    amend

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -25,7 +25,7 @@ class MyRunnable implements Runnable {
     @Override
     public void run() {
 
-        for (int i = 0; i < 20; i++) {
+        for (int i = 0; i < 200; i++) {
             System.out.println(Thread.currentThread().getName() + "_____________________" + i);
         }
     }
