82fc19532e8e2a5be0d1a0b35ed71d23cdf0ab9a
YOUR NAME <YOU@EXAMPLE.COM>
2017-10-11 22:22:23 +0800

    添加测试

--- src/test/java/testHelloWorld/HelloWorld.java
+++ src/test/java/testHelloWorld/HelloWorld.java
@@ -1,12 +1,40 @@
 package testHelloWorld;
 
+import org.junit.*;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.util.List;
+
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
 /**
  * Created by WangDi on 2017/7/26.
  */
 public class HelloWorld {
 
-    public static void main(String[] args) {
+    @Test
+    public void run(){
 
+        List list = mock(List.class);
+        list.add(1);
+        list.clear();
 
+        verify(list).add(1);
+        verify(list).clear();
+    }
+
+    @Test
+    public void when_throw() throws IOException {
+
+        OutputStream outputStream = mock(OutputStream.class);
+        OutputStreamWriter outputStreamWriter = new OutputStreamWriter(outputStream);
+
+        //预设流抛出异常
+        doThrow(new IOException()).when(outputStream).close();
+        outputStream.close();
     }
 }
