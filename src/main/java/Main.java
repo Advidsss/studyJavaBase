641448add2b09745746c0cb0d0d2e60d43243fdb
alonesink <alonesink0727@163.com>
2018-07-04 21:31:51 +0800

    添加说明

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,7 +11,23 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        Scanner scanner1 = new Scanner(System.in);
+        String str = scanner.nextLine();
+        String str1 = scanner1.nextLine();
 
+        int length = Math.min(str.length(),str1.length());
+        int pos = 0;
+
+        while (pos < length){
+            if(0 != (str.charAt(pos)^str1.charAt(pos))){
+                break;
+            }
+            pos++;
+        }
+
+        System.out.println(str.substring(pos));
+        System.out.println(str1.substring(pos));
     }
 
 }
